6ad513f094043b53dff8e254d179dc376fcc5335
Bilal MB <bmbenkada@gmail.com>
2022-09-01 11:25:04 +0100

    Implementing addEventListener

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict"
 
 let inputButton = document.getElementById("input-btn")
+inputButton.addEventListener("click", function() {
+    console.log("Button clicked from event listener!")
+})
 
 function saveLead() {
-    console.log("Button clicked!")
+    console.log("Button clicked from onclick attribute!")
 }
