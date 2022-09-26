b5671ea56c3ef2bfe932012857c8f3f122764665
Bilal MB <bmbenkada@gmail.com>
2022-09-26 17:31:16 +0100

    Ill for two weeks but back developing the chrome extension. Fixed input box to allow user input to be shown and is now clickable

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict"
-let myLeads = ["www.1.com", "www.2.com", "www.3.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -7,16 +7,17 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-for (let i=0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
+function renderLeads() {
+    let listItems = ""
+    for (let i=0; i < myLeads.length; i++) {
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    }
+    ulEl.innerHTML = listItems
 }
-
 
 /////////////////// addEventListener ///////////////////
 // let myBox = document.getElementById("box-el")
